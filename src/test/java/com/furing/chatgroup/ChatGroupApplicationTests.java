57887fda163445057a4a89ba2f3893d3134c5afb
furing <913845140@qq.com>
2023-10-26 15:35:30 +0800

    Initial commit

---
+++ src/test/java/com/furing/chatgroup/ChatGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furing.chatgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
